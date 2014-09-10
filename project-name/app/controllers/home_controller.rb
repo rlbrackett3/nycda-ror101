08161ae86c7940e2202a73aed6408c402ed0af38
Robert Lee Brackett III <robert@miscproj.com>
2014-09-10 18:44:33 -0400

    lesson 2 stuff

--- project-name/app/controllers/home_controller.rb
+++ project-name/app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
-  def index
 
-  end
+  def index
+    @transactions = [
+                      { amount: 10, date: Time.now },
+                      { amount: 20, date: Time.now - 2.days }
+                    ]
+    end
 
   def about
 
